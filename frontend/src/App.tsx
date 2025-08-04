fef9a1f27742d67ccef05aaf5e92537563ddd39c
Axel <yoloax11@gmail.com>
2025-08-03 21:36:01 -0700

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '@/pages/Home'
 import Navigation from '@/components/nav'
+import { Footer } from '@/components/footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
