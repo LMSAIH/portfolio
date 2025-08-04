1f9838beaad64105792a245fbf97dc95a6abe5db
Axel <yoloax11@gmail.com>
2025-08-03 21:36:29 -0700

    created the footer component

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+    return (
+        <footer className="mt-16 py-8 border-t border-gray-200 dark:border-gray-700">
+            <div className="px-4 mx-auto max-w-7xl">
+                <div className="text-center text-sm text-gray-600 dark:text-gray-400">
+                    <p>
+                        <span className="font-medium">Axel Velasquez &copy; {new Date().getFullYear()}</span>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
